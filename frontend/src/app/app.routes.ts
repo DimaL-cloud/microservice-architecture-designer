3d3949dc6cb548fca982c41a7817747e54593c8f
DLutsyuk <dimaconsi@gmail.com>
2026-05-25 21:12:05 +0300

    title

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Projects',
     loadComponent: () =>
       import('./projects/project-list').then(m => m.ProjectList)
   },
